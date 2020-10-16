e6c6311fe64e36a055e8a08c007a1ecc692948ac
Mariana Coelho <coelhomariana@outlook.com>
2020-10-16 17:58:09 -0300

    Adicionando modelo e primeira rota de criar orfanato

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,10 @@
 import express from 'express';
-
 import './database/connection'
+import routes from './routes.ts'
 
 const app = express()
 
-app.get('/users', (request, response) => {
-    return response.json({message: 'Hello World'})
-})
+app.use(express.json());
+app.use(routes)
 
 app.listen(3333);
