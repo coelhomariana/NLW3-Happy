9fa9240f2906667bd3476eb62974d2317bc3d0b4
Mariana Coelho <coelhomariana@outlook.com>
2020-10-20 17:48:07 -0300

    feita a rota de post para criar orfanato

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,37 @@
 import express from 'express';
 import './database/connection'
-import routes from './routes.ts'
+import {getRepository} from 'typeorm';
+import Orphanage from './models/Orphanage'
 
 const app = express()
 
 app.use(express.json());
-app.use(routes)
+app.post('/orphanages', async (request, response) => {
+    const {
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions, 
+        opening_hours,
+        open_on_weekends
+    } = request.body;
+
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanage = orphanagesRepository.create({
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions, 
+        opening_hours,
+        open_on_weekends
+    })  
+
+    await orphanagesRepository.save(orphanage)
+
+    return response.json({message: "Hello world"})
+})
 
 app.listen(3333);
