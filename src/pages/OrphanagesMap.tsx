411ee73bc6401a876cb1e96cdeabbc3712db08c6
Mariana Coelho <coelhomariana@outlook.com>
2020-11-09 18:44:33 -0300

    Colocando markers no mapa

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -3,8 +3,17 @@ import mapMarker from "../images/map-marker.svg";
 import { Link } from "react-router-dom";
 import { FiPlus } from "react-icons/fi";
 import "../styles/orphanages-map.css";
-import { Map, TileLayer } from "react-leaflet";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import Leaflet from "leaflet";
+import mapMarkerImg from "../images/map-marker.svg";
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2]
+});
 
 function OrphanagesMap() {
   return (
@@ -29,6 +38,14 @@ function OrphanagesMap() {
         style={{ width: "100%", height: "100%" }}
       >
         <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+        <Marker icon={mapIcon} position={[-30.0519427, -51.2317813]}>
+          <Popup closeButton={false} minWidth={248} maxWidth={240} className="map-popup">
+              'Lar das meninas'
+          </Popup>
+        </Marker>
+        
+        />
       </Map>
 
       <Link to="" className="create-orphanage">
